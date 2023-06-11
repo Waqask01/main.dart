a2ceaf4ae6ca039efbc91f0ee51b39869cb9ad93
Bilal <asdasjh@gmail.com>
2023-06-11 17:27:25 +0500

    create file

---
+++ main.dart
@@ -0,0 +1,15 @@
+void main() {
+  var email = ("waqas01@gmail.com");
+  var Password = ("123456");
+  If (email == "waqas01@gmail.com" && password == "123456"){
+    print ("Login is correct");
+    }
+    else {
+     if (email != waqas01@gmail.com) {
+    print ("email galat hai"); 
+    }
+    else (password != 123456){ 
+      print ("password is wrong");
+    }
+  }
+}
