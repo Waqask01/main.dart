55202611ff7973e547ff5c9d539c1c5b3226aeca
Waqask01 <136242581+Waqask01@users.noreply.github.com>
2023-06-25 14:45:02 +0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,15 +1,15 @@
 void main() {
   var email = ("waqas01@gmail.com");
-  var Password = ("123456");
-  If (email == "waqas01@gmail.com" && password == "123456"){
+  var password = ("123456");
+  if (email == "waqas01@gmail.com" && password == "123456"){
     print ("Login is correct");
     }
     else {
-     if (email != waqas01@gmail.com) {
+    if (email != "waqas01@gmail.com") {
     print ("email galat hai"); 
     }
-    else (password != 123456){ 
-      print ("password is wrong");
-    }
-  }
+    else (password != "123456");
+      { 
+      print("password is wrong");   
+    }  
 }
